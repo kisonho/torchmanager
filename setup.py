c22867a83b7ad4d0a13990e43b8410437301944e
Qisheng He <RobertHe.KsHo@wayne.edu>
2022-01-19 13:43:56 -0500

    01-19-22
    - setup file update

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ setup(name='torchmanager',
       version='0.9.0',
       description='PyTorch training manager',
       author='Kison Ho',
-      author_email='RobertHe.KsHo@wayne.edu',
+      author_email='unfit-gothic.0q@icloud.com',
       packages=['torchmanager'],
       package_dir={
             'torchmanager': 'lib'
-      }
+      },
+      url="https://github.com/kisonho/torchmanager.git"
 )
