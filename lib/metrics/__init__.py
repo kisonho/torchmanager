2e12fe4cc9fbba7f22f3f072feb2b39f5ec5b2c7
Qisheng He <RobertHe.KsHo@wayne.edu>
2022-03-31 11:30:17 -0400

    03-31-22
    - Minor bugs fixded

--- lib/metrics/__init__.py
+++ lib/metrics/__init__.py
@@ -1,3 +1,3 @@
 from .accuracy import Accuracy, CategoricalAccuracy, MAE, SparseCategoricalAccuracy
-from conf_met import ConfusionMetrics, MIoU
+from .conf_met import ConfusionMetrics, MIoU
 from .metrics import Metric, metric
