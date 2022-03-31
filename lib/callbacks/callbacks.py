681589a1d25ff5432149b6fad4f2ab2e00a109bd
Qisheng He <RobertHe.KsHo@wayne.edu>
2022-03-31 11:21:01 -0400

    03-31-22
    - LrScheduler callback add to summary writer

--- lib/callbacks/callbacks.py
+++ lib/callbacks/callbacks.py
@@ -147,20 +147,38 @@ class LrSchedueler(Callback):
         - freq: An `_lr.LrScheduleFreq` of the frequency to update learning rate
     """
     __lr_scheduler: torch.optim.lr_scheduler._LRScheduler
+    _writer: Optional[tensorboard.writer.SummaryWriter]
     freq: _lr.LrScheduleFreq
 
-    def __init__(self, scheduler: torch.optim.lr_scheduler._LRScheduler, freq: _lr.LrScheduleFreq) -> None:
+    def __init__(self, scheduler: torch.optim.lr_scheduler._LRScheduler, freq: _lr.LrScheduleFreq, tf_board_writer: Optional[tensorboard.writer.SummaryWriter] = None) -> None:
         super().__init__()
         self.__lr_scheduler = scheduler
+        self._writer = tf_board_writer
         self.freq = freq
 
     def on_batch_end(self, *args, **kwargs) -> None:
         if self.freq == _lr.LrScheduleFreq.BATCH:
             self.__lr_scheduler.step()
 
-    def on_epoch_end(self, *args, **kwargs) -> None:
+    def on_epoch_end(self, epoch: int, *args, **kwargs) -> None:
+        # update lr scheduler
         if self.freq == _lr.LrScheduleFreq.EPOCH:
             self.__lr_scheduler.step()
+
+        # get summary
+        summary = {}
+        lr_list = self.__lr_scheduler.get_last_lr()
+        if len(lr_list) > 1:
+            for i, lr in enumerate(lr_list):
+                summary[f'lr_{i}'] = lr
+        else: summary['lr'] = lr_list[0]
+        
+        # write results to Tensorboard
+        if self._writer is not None:
+            for key in summary.keys():
+                result: Dict[str, float] = {}
+                result["train"] = summary[key]
+                self._writer.add_scalars(key, result, epoch + 1)
 
 class TensorBoard(Callback):
     """
