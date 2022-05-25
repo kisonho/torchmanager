d0f55d035bbf74fcffd3bb1280f4b7064d084219
Qisheng He <RobertHe.KsHo@wayne.edu>
2022-05-25 13:43:04 -0400

    05-25-22
    - Version compatibility fixed

---
+++ lib/metrics/metrics.py
@@ -0,0 +1,5 @@
+from torchmanager_core.view import warnings
+
+from .metric import *
+
+warnings.warn("This module has been deprecated after v1.0.4 and will be removed in v1.2.0, use `metric` instead.", PendingDeprecationWarning)
