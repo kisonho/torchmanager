4a4258ff73b828c7cde543fb4a0d0760145cc61c
Qisheng He <RobertHe.KsHo@wayne.edu>
2022-06-09 10:33:13 -0400

    03-14-22
    - Multi losses bugs fixed

--- lib/losses.py
+++ lib/losses.py
@@ -92,7 +92,10 @@ class MultiLosses(Loss):
 
         # get all losses
         for fn in self.losses:
-            loss += fn.forward(input, target)
+            assert isinstance(fn, Loss), f"[Runtime Error]: Function {fn} is not a Loss object."
+            l = fn(input, target)
+            loss = loss.to(l.device)
+            loss += l
         return loss
 
 class MultiOutputsLosses(Loss):
