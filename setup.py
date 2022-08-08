a05fc108d88f4678e3277a403eb885f8d146f9a6
Qisheng He <RobertHe.KsHo@wayne.edu>
2022-08-08 17:26:57 -0400

    08-08-22
    - v1.0.5 built

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(name='torchmanager',
-    version='1.0.5b6',
-    description='PyTorch training manager (v1.0.5 Beta 6)',
+    version='1.0.5',
+    description='PyTorch training manager (v1.0.5)',
     long_description=long_description,
     long_description_content_type='text/markdown',
     author='Kison Ho',
