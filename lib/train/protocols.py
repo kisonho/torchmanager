90b2fa72096eaa2e660b7f41786a00a1ab058ada
Qisheng He <RobertHe.KsHo@wayne.edu>
2022-09-26 10:55:37 -0400

    09-26-22
    - Minor bugs fixed

--- lib/train/protocols.py
+++ lib/train/protocols.py
@@ -2,6 +2,13 @@ from torchmanager_core import abc
 from torchmanager_core.typing import Any, List, OrderedDict, Protocol, Optional
 from torchmanager_core.view.protocols import VerboseControllable
 
+class LrSteping(Protocol):
+    @abc.abstractmethod
+    def get_last_lr(self) -> List[float]: return NotImplemented
+
+    @abc.abstractmethod
+    def step(self, epoch: Optional[int] = None) -> None: pass
+
 class StateDictLoadable(Protocol):
     """An object that can load state dict"""
     @abc.abstractmethod
@@ -9,10 +16,3 @@ class StateDictLoadable(Protocol):
 
     @abc.abstractmethod
     def state_dict() -> OrderedDict[str, Any]: return NotImplemented
-
-class LrSteping(Protocol):
-    @abc.abstractmethod
-    def get_last_lr(self) -> List[float]: return NotImplemented
-
-    @abc.abstractmethod
-    def step(self, epoch: Optional[int] = None) -> None: pass
