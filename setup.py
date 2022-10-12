dd7e37446e313c22217694ba6424ffdc83b22fa5
Qisheng He <RobertHe.KsHo@wayne.edu>
2022-10-12 09:47:07 -0400

    10-12-22
    - Minor bugs fixed

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(name='torchmanager',
-    version='1.0.6b10',
-    description='PyTorch training manager (v1.0.6 Beta 10)',
+    version='1.0.6b11',
+    description='PyTorch training manager (v1.0.6 Beta 11)',
     long_description=long_description,
     long_description_content_type='text/markdown',
     author='Kison Ho',
