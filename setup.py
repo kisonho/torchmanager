3fcbe436f713b1932fcb187e7ef6e8ef1a48f644
Qisheng He <RobertHe.KsHo@wayne.edu>
2022-12-16 15:46:13 -0500

    12-16-22
    - Version number updated

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(name='torchmanager',
-    version='1.0.7',
-    description='PyTorch training manager (v1.0.7)',
+    version='1.0.7b1',
+    description='PyTorch training manager (v1.0.7 Beta 1)',
     long_description=long_description,
     long_description_content_type='text/markdown',
     author='Kison Ho',
