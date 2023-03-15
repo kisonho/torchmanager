6bca75448fc3fbee321879c27aa437ba7775fd2c
Qisheng He <RobertHe.KsHo@wayne.edu>
2023-03-15 13:41:15 -0400

    03-15-23
    - Minor bugs fixed

--- core/random/seed.py
+++ core/random/seed.py
@@ -1,6 +1,6 @@
 import numpy as np, random, torch
 
-from torchmanager_core import devices
+from .. import devices
 
 def freeze_seed(seed: int) -> None:
     """
@@ -22,10 +22,15 @@ def unfreeze_seed() -> int:
 
     - Returns: An `int` of random seed
     """
-    seed = torch.random.seed()
-    random.seed()
-    np.random.seed()
+    # random PyTorch seed
+    seed_64 = torch.random.seed()
+    
+    # set 32 bit seed for random and np package
+    seed_32 = int(seed_64 / (2 ** 32)) if seed_64 > 2 ** 32 - 1 else seed_64 # convert to 32 bit seed
+    random.seed(seed_32)
+    np.random.seed(seed_32)
 
+    # set cuda seed
     if devices.GPU is not NotImplemented:
-        torch.cuda.manual_seed_all(seed)
-    return seed
+        torch.cuda.manual_seed_all(seed_64)
+    return seed_64
--- lib/train/__init__.py
+++ lib/train/__init__.py
@@ -1,3 +1,3 @@
 from torchmanager_core.protocols import Frequency as LrScheduleFreq
 from .checkpoint import Checkpoint, list_checkpoints, load
-from .learning_rate import initial_step_lr_scheduler, update_lr
+from .learning_rate import update_lr
