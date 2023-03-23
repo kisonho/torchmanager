a94cb74f34ad0229583ece22baee5120d707fe96
Qisheng He <RobertHe.KsHo@wayne.edu>
2023-03-23 18:01:12 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(name='torchmanager-nightly',
-    version="v1.2a5",
+    version="1.2a5",
     description="PyTorch Training Manager v1.2 (Alpha 5)",
     long_description=long_description,
     long_description_content_type='text/markdown',
