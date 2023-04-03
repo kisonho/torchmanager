1664b967698121e14ea1be0431ddbfc6b8947404
Qisheng He <RobertHe.KsHo@wayne.edu>
2023-04-03 11:12:49 -0400

    04-03-23
    - Minor bugs fixed

--- lib/metrics/fid.py
+++ lib/metrics/fid.py
@@ -1,23 +1,22 @@
 from torchmanager_core import torch
-from torchmanager_core.typing import List, Optional, Self
-from torchvision.models import inception_v3
+from torchmanager_core.typing import Generic, List, Module, Optional
 
 from .metric import Metric
 
 
-class FID(Metric):
+class FID(Metric, Generic[Module]):
     """
     Fréchet Inception Distance (FID) metric
 
     - Parameters:
-        - feature_extractor: A `torch.nn.Module` to extract the features
+        - feature_extractor: A `Module` to extract the features
         - input_features: A `list` of current features for generated images that extracted in `torch.Tensor`
         - result: A `torch.Tensor` of the current FID result
         - return_when_forwarding: A `bool` flag of if returning results when calculating the metrics, a `torch.nan` will be returned if set to `False` during forwarding
         - target_features: A `list` of current features for real images that extracted in `torch.Tensor`
     """
-    feature_extractor: torch.nn.Module
-    """A `torch.nn.Module` to extract the features"""
+    feature_extractor: Optional[Module]
+    """A `Module` to extract the features"""
     input_features: List[torch.Tensor]
     """A `list` of current features for generated images that extracted in `torch.Tensor`"""
     return_when_forwarding: bool
@@ -27,7 +26,12 @@ class FID(Metric):
 
     @property
     def result(self) -> torch.Tensor:
-        """A `torch.Tensor` of the current FID result"""
+        """A `torch.Tensor` of the mean of current FID result"""
+        return self.results.mean()
+    
+    @property
+    def results(self) -> torch.Tensor:
+        """A `torch.Tensor` of the current FID results"""
         # concat input and target
         input = torch.cat(self.input_features)
         target = torch.cat(self.target_features)
@@ -37,44 +41,31 @@ class FID(Metric):
         mu_gen = input.mean(0)
         sigma_real = target.cov()
         sigma_gen = input.cov()
+        diff = mu_real - mu_gen
 
         # Calculate the squared Euclidean distance between the means
-        diff = (mu_real - mu_gen) ** 2
-        sigma = torch.trace(sigma_real) + torch.trace(sigma_gen) - 2 * torch.trace((torch.dot(sigma_real, sigma_gen).sqrt()))
-        fid_score = diff + sigma
-        return fid_score
+        return diff @ diff + torch.trace(sigma_real + sigma_gen - 2 * (sigma_real * sigma_gen).sqrt())
 
-    def __init__(self, feature_extractor: Optional[torch.nn.Module] = None, return_when_forwarding: bool = True, target: Optional[str] = None) -> None:
+    def __init__(self, feature_extractor: Optional[Module] = None, return_when_forwarding: bool = True, target: Optional[str] = None) -> None:
         """
         Constructor
 
         - Parameters:
-            - feature_extractor: An optional `torch.nn.Module` to extract features, a pre-trained InceptionV3 will be used if not given
+            - feature_extractor: An optional `Module` to extract features, a pre-trained InceptionV3 will be used if not given
             - return_when_forwarding: A `bool` flag of if returning results when calculating the metrics, a `torch.nan` will be returned if set to `False` during forwarding
             - target: A `str` of target name in `input` and `target` during direct calling
         """
-        super(FID, self).__init__(target=target)
-        self.return_when_forwarding = return_when_forwarding
+        super().__init__(target=target)
+        self.feature_extractor = feature_extractor
         self.input_features = []
+        self.return_when_forwarding = return_when_forwarding
         self.target_features = []
-
-        # initialize inception model
-        if feature_extractor is None:
-            self.feature_extractor = inception_v3(pretrained=True, transform_input=False)
-            self.feature_extractor.fc = torch.nn.Identity()  # Remove last layer # type: ignore
-            self.feature_extractor.eval()
-        else:
-            self.feature_extractor = feature_extractor
-
-        # disable gradients
-        for param in self.feature_extractor.parameters():
-            param.requires_grad = False
 
     def forward(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
         input_features = self.forward_features(input)
         target_features = self.forward_features(target)
-        self.input_features.append(input_features)
-        self.target_features.append(target_features)
+        self.input_features += [f.unsqueeze(0).cpu().detach() for f in input_features]
+        self.target_features += [f.unsqueeze(0).cpu().detach() for f in target_features]
         return self.result if self.return_when_forwarding else torch.tensor(torch.nan)
 
     @torch.no_grad()
@@ -84,14 +75,15 @@ class FID(Metric):
 
         - Parameters:
             - x: A `torch.Tensor` to extract
-        - Returns: A `torch.Tensor` of features
+        - Returns: A `torch.Tensor` of features if feature extractor is given
         """
         # get features]
-        features: torch.Tensor = self.feature_extractor(x)
-        features = features.squeeze(3).squeeze(2)
-        return features
+        if self.feature_extractor is not None:
+            return self.feature_extractor(x)
+        else:
+            return x
     
-    def train(self, mode: bool = True) -> Self:
+    def train(self, mode: bool = True):
         self.training = mode
         return self
 
