22bf8c628c444926a369e8b9235ff3aabde5a051
Qisheng He <Qisheng.He@wayne.edu>
2023-05-23 15:15:47 -0400

    Minor bugs fixed

--- torchmanager/metrics/__init__.py
+++ torchmanager/metrics/__init__.py
@@ -3,3 +3,4 @@ from .conf_met import BinaryConfusionMetric, ConfusionMetrics, Histogram
 from .extractor import ExtractorScore, FeatureMetric, FID
 from .iou import InstanceIoU, MeanIoU
 from .metric import Metric, Reduction, metric, metric_fn
+from .similarity import SSIM
