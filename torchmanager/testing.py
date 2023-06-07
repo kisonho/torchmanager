f40d8c827693705105979104d1bda34e41b074ba
Qisheng He <Qisheng.He@wayne.edu>
2023-06-07 14:36:39 -0400

    Minor bugs fixed

--- torchmanager/testing.py
+++ torchmanager/testing.py
@@ -39,7 +39,9 @@ class Manager(BaseManager[Module]):
 
         # summarize metrics
         for name, fn in self.metric_fns.items():
-            if name.startswith("val_"):
+            if name.startswith("val_") and self.model.training:
+                continue
+            elif name.startswith("val_"):
                 name = name.replace("val_", "")
             try:
                 summary[name] = float(fn.result.detach())
