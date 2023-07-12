5b311552685f50776a6867d949b2a126951acd7a
Qisheng He <Qisheng.He@wayne.edu>
2023-07-12 14:11:46 -0400

    Move optimizer steps outside of `backward` method

--- torchmanager/training.py
+++ torchmanager/training.py
@@ -137,9 +137,7 @@ class Manager(_Manager[Module]):
         - Parameters:
             - loss: A `torch.Tensor` of loss value
         """
-        self.compiled_optimizer.zero_grad()
         loss.backward()
-        self.compiled_optimizer.step()
 
     def fit(self, training_dataset: Union[DataLoader[Any], Dataset[Any], Collection], /, epochs: Optional[int] = None, val_dataset: Optional[Union[DataLoader[Any], Dataset[Any], Collection]] = None, callbacks_list: List[Callback] = [], *, iterations: Optional[int] = None, initial_epoch: Optional[int] = None, device: Optional[Union[torch.device, List[torch.device]]] = None, use_multi_gpus: bool = False, **kwargs) -> Module:
         """
@@ -265,7 +263,9 @@ class Manager(_Manager[Module]):
                 fn(y, y_train)
 
         # backward pass
+        self.compiled_optimizer.zero_grad()
         self.backward(loss)
+        self.compiled_optimizer.step()
         return self.summary
 
     def to_checkpoint(self) -> Checkpoint[Self]:
