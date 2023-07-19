8f70f1638254c4a1788d11746a0b479cd4374aa6
Qisheng He <Qisheng.He@wayne.edu>
2023-07-19 12:13:05 -0400

    Minor bugs fixed

--- tests/test_0102.py
+++ tests/test_0102.py
@@ -4,9 +4,23 @@ from unittest import TestCase
 
 
 class Test0102(TestCase):
+    def test_dice_metric(self) -> None:
+        from torchmanager import metrics
+
+        # initialize
+        dice_score_fn = metrics.Dice()
+        y = torch.randn((4, 4, 224, 224))
+        y_test = torch.randn_like(y)
+
+        # test dice score
+        dice = dice_score_fn(y, y_test)
+        self.assertGreaterEqual(float(dice), 0, f"Dice value must be non-negative, got {dice}.")
+        self.assertLessEqual(float(dice), 1, f"Dice value must be less or equal to 1, got {dice}.")
+
     def test_random(self) -> None:
         from torchmanager_core.random import freeze_seed, unfreeze_seed
 
+        # freeze seed
         seed = 1
         freeze_seed(1)
         random_num_1 = torch.randint(0, 1, [1])
@@ -14,6 +28,7 @@ class Test0102(TestCase):
         random_num_2 = torch.randint(0, 1, [1])
         self.assertEqual(int(random_num_1), int(random_num_2))
 
+        # unfreeze seed
         random_seed = unfreeze_seed()
         self.assertNotEqual(seed, random_seed, f"Seed not unfrozon: current_seed={seed}")
 
