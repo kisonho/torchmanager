ccc5d575df67a16c43111518804f80490618fa5f
Qisheng He <Qisheng.He@wayne.edu>
2023-07-26 17:16:18 -0400

    Minor bugs fixed

--- torchmanager/training.py
+++ torchmanager/training.py
@@ -174,7 +174,7 @@ class Manager(_Manager[Module]):
 
         # initialize training
         cpu, device, target_devices = devices.search(device)
-        if device == cpu and len(target_devices) < 2:
+        if device == cpu or len(target_devices) < 2:
             use_multi_gpus = False
         devices.set_default(target_devices[0])
         if lr_scheduler is not None:
