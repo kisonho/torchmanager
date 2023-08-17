d87991b18b0da5d5e4db3b8f596243fd7bad6247
Qisheng He <Qisheng.He@wayne.edu>
2023-08-17 12:21:49 -0400

    Minor bugs fixed

--- torchmanager/testing.py
+++ torchmanager/testing.py
@@ -109,7 +109,7 @@ class Manager(BaseManager[Module]):
         """
         # initialize device
         cpu, device, target_devices = devices.search(device)
-        if device == cpu and len(target_devices) < 2:
+        if device == cpu or len(target_devices) < 2:
             use_multi_gpus = False
         devices.set_default(target_devices[0])
 
