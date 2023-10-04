c48b399dec200226415f0c816163cfa7acc86429
Qisheng He <QishengRobertHe@outlook.com>
2023-10-04 10:18:26 -0400

    Update conf_met.py
    - Add class index control for `metrics.BinaryConfusionMetric`

--- torchmanager/metrics/conf_met.py
+++ torchmanager/metrics/conf_met.py
@@ -1,5 +1,5 @@
 from torch.nn import functional as F
-from torchmanager_core import abc, torch, deprecated, _raise
+from torchmanager_core import Version, abc, torch, deprecated, _raise
 from torchmanager_core.typing import Any, Optional, Tuple
 
 from .metric import Metric
@@ -15,34 +15,38 @@ class BinaryConfusionMetric(Metric, abc.ABC):
     - Methods to implement:
         - forward_metric: The main method that accepts TP, TN, FP, and FN as `torch.Tensor` and returns the final metric as `torch.Tensor`
     """
+    _class_index: int
     _dim: int
     _eps: float
 
-    def __init__(self, dim: int = 1, *, eps: float=1e-7, target: Optional[str] = None):
+    def __init__(self, dim: int = 1, *, class_index: int = 1, eps: float=1e-7, target: Optional[str] = None):
         """
         Constructor
 
         - Parameters:
             - dim: The class channel dimmension index in `int`
+            - class_index: The target class index in `int`
             - eps: A `float` of the small number to avoid zero divide
             - target: A `str` of target name in `input` and `target` during direct calling
         """
         super().__init__(target=target)
+        self._class_index = class_index
         self._dim = dim
         self._eps = eps
 
+    def convert(self, from_version: Version) -> None:
+        return super().convert(from_version)
+
     def forward(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
         # argmax input
-        num_classes = input.shape[self._dim]
         input = input.argmax(dim=self._dim) if input.shape[self._dim] > 1 else input > 0.5
 
-        # check if multi classes
-        if num_classes > 2:
-            input = F.one_hot(input, num_classes)
-            target = F.one_hot(target, num_classes)
+        # mask input and target
+        input = input == self._class_index
+        target = target == self._class_index
 
         # calculate TP, FP, and FN
-        tp, tn, fp, fn = self.forward_conf_met(input, target)
+        tp, tn, fp, fn = self.forward_conf_met(input.int(), target.int())
         return self.forward_metric(tp, tn, fp, fn)
 
     def forward_conf_met(self, input: torch.Tensor, target: torch.Tensor) -> Tuple[torch.Tensor, ...]:
