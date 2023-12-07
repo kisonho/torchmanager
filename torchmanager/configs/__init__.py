b6982fcab8d81e8777f8779f9e08792af22332ef
Qisheng He <Qisheng.He@wayne.edu>
2023-12-07 11:11:47 -0500

    Optional import improvement

--- torchmanager/configs/__init__.py
+++ torchmanager/configs/__init__.py
@@ -4,4 +4,6 @@ from .json import JSONConfigs
 try:
     from .yaml import YAMLConfigs
 except ImportError:
+    from torchmanager_core import view
+    view.warnings.warn("PyYAML dependency is not installed, install it to use `YAMLConfigs`.", ImportWarning)
     YAMLConfigs = NotImplemented
