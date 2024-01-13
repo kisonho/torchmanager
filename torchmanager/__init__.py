93865506f42fab7b49167d1e248a13801e09cc43
Qisheng Robert He <Qisheng.He@wayne.edu>
2024-01-12 19:58:43 -0500

    Compatibility fixed

--- torchmanager/__init__.py
+++ torchmanager/__init__.py
@@ -4,4 +4,6 @@ from torchmanager_core.view import VerboseType
 from . import callbacks, configs, data, losses, metrics
 from .testing import Manager as TestingManager
 from .training import Manager as TrainingManager
+
 Manager = TrainingManager
+version = VERSION
