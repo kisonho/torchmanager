ced7f91041ff888612be5b7286bfdb9e0507572e
Qisheng He <Qisheng.He@wayne.edu>
2024-07-22 17:33:38 -0400

    07-22-24
    - Deprecation function can now takes a `None` variable as its undecided removing version

--- torchmanager_core/version/deprecation.py
+++ torchmanager_core/version/deprecation.py
@@ -1,11 +1,13 @@
 import functools, warnings
-from typing import Any
+from typing import Optional, TypeVar, Union
 
 from .details import CURRENT
 from .errors import VersionError
 from .version import Version
 
-def deprecated(target_version: Any, removing_version: Any, *, current_version: Any = CURRENT):
+VersionType = Union[str, Version]
+
+def deprecated(target_version: VersionType, removing_version: Optional[VersionType], *, current_version: VersionType = CURRENT):
     '''
     Deprecated decorator function
 
@@ -15,19 +17,24 @@ def deprecated(target_version: Any, removing_version: Any, *, current_version: A
     '''
     # format versions
     target_version = Version(target_version)
-    removing_version = Version(removing_version)
+    removing_version = Version(removing_version) if removing_version is not None else None
     current_version = Version(current_version)
 
     # define wrapping function
     def wrapping_fn(fn):
         @functools.wraps(fn)
         def deprecated_fn(*args, **kwargs):
-            if current_version >= removing_version:
-                raise VersionError(fn.__name__, removing_version)
+            # check version
+            if removing_version is None and current_version < target_version:
+                warnings.warn(f"{fn} will be deprecated from {target_version} and removed from a future version.", PendingDeprecationWarning)
+            elif removing_version is None and current_version >= target_version:
+                warnings.warn(f"{fn.__name__} has been deprecated from {target_version} and will be removed from a future version", DeprecationWarning)
+            elif removing_version is not None and current_version < target_version:
+                warnings.warn(f"{fn} will be deprecated from {target_version} and removed from {removing_version}", PendingDeprecationWarning)
             elif current_version >= target_version:
                 warnings.warn(f"{fn.__name__} has been deprecated from {target_version} and will be removed from {removing_version}", DeprecationWarning)
             else:
-                warnings.warn(f"{fn} will be deprecated from {target_version} and removed from {removing_version}", PendingDeprecationWarning)
+                raise VersionError(fn.__name__, str(removing_version))
             return fn(*args, **kwargs)
         return deprecated_fn
     return wrapping_fn
