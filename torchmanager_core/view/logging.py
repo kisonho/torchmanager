90145bda76a3c3256bc0d2aa6139fb09f34b5dce
Qisheng He <Qisheng.He@wayne.edu>
2025-03-05 10:44:37 -0500

    Minor bugs fixed

--- torchmanager_core/view/logging.py
+++ torchmanager_core/view/logging.py
@@ -37,12 +37,21 @@ def set_log_path(log_path: str) -> Formatter:
     - Returns: A `logging.Formatter` of log formatter with the log path
     """
     # initialize formatter
+    handlers = logger.handlers
     formatter = Formatter("%(message)s")
+    contains_file_handler = any(isinstance(handler, FileHandler) for handler in handlers)
 
     # loop through handlers
-    for i, handler in enumerate(logger.handlers):
-        if isinstance(handler, FileHandler):
-            handler.baseFilename = log_path
-            handler.setFormatter(formatter)
-            logger.handlers[i] = handler
+    if contains_file_handler:
+        for i, handler in enumerate(handlers):
+            if isinstance(handler, FileHandler):
+                handler.baseFilename = log_path
+                handler.setFormatter(formatter)
+                logger.handlers[i] = handler
+                break
+    else:
+        file_handler = FileHandler(log_path)
+        file_handler.setLevel(INFO)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
     return formatter
