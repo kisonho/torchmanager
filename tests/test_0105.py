932b618bee1a1097ff9319211187c712765b8659
Qisheng Robert HE, PhD <Qisheng.He@hfhs.org>
2026-04-30 14:00:30 -0400

    Minor bugs fixed.

--- tests/test_0105.py
+++ tests/test_0105.py
@@ -11,3 +11,23 @@ class Test0105(TestCase):
     def test_version(self) -> None:
         from torchmanager_core import API_VERSION
         self.assertGreaterEqual(API_VERSION, "1.5")
+
+    def test_lpips(self) -> None:
+        from torchmanager.metrics.lpips import LPIPS, LPIPSNetType
+        from torchmanager_core import torch
+
+        alex = torch.nn.Sequential(*[torch.nn.Identity() for _ in range(12)])
+        squeeze = torch.nn.Sequential(*[torch.nn.Identity() for _ in range(13)])
+        vgg = torch.nn.Sequential(*[torch.nn.Identity() for _ in range(30)])
+
+        self.assertEqual(len(LPIPSNetType.ALEX.load(alex)), 5)
+        self.assertEqual(len(LPIPSNetType.SQUEEZE.load(squeeze)), 7)
+        self.assertEqual(len(LPIPSNetType.VGG16.load(vgg)), 5)
+
+        metric = LPIPS()
+        input_features = [torch.ones((2, 3, 4, 4))]
+        target_features = [-torch.ones((2, 3, 4, 4))]
+
+        result = metric(input_features, target_features)
+
+        self.assertTrue(torch.isclose(result, torch.tensor(4.0)))
